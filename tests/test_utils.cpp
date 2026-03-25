71cf1a20790af61fcd9bb4050ca722087aa0a902
OrangeSmokingjacket <whereisgeol@gmail.com>
2026-03-25 18:23:43 +0100

    ub consistency tests

--- tests/test_utils.cpp
+++ tests/test_utils.cpp
@@ -3,6 +3,8 @@
 #include "quadruple.hpp"
 #include <random>
 
+#include "ub_consistency.hpp"
+
 template <typename T>
 bool is_negative(T val) {
     return std::signbit(val);
@@ -12,9 +14,67 @@ template <>
 bool is_negative(quadruple val) {
     return val.signbit();
 }
+TEMPLATE_TEST_CASE("UB consistency", "[utils]",
+    int8_t,
+    int16_t,
+    int32_t,
+    int64_t,
+    uint8_t,
+    uint16_t,
+    uint32_t,
+    uint64_t) {
+    SECTION("positive") {
+        SECTION("overflow") {
+            double val = std::numeric_limits<double>::max();
+            TestType converted = static_cast<TestType>(val);
+            REQUIRE(converted == UB_handle::to_integer_conversion::POS_OVERFLOW<TestType>);
+        }
+        SECTION("infinity") {
+            double val = std::numeric_limits<double>::infinity();
+            TestType converted = static_cast<TestType>(val);
+            REQUIRE(converted == UB_handle::to_integer_conversion::POS_INF<TestType>);
+        }
+        SECTION("NaN") {
+            SECTION("quiet") {
+                double val = std::numeric_limits<double>::quiet_NaN();
+                TestType converted = static_cast<TestType>(val);
+                REQUIRE(converted == UB_handle::to_integer_conversion::NaN<TestType>);
+            }
+            SECTION("signaling") {
+                double val = std::numeric_limits<double>::signaling_NaN();
+                TestType converted = static_cast<TestType>(val);
+                REQUIRE(converted == UB_handle::to_integer_conversion::NaN<TestType>);
+            }
+        }
+    }
+
+    SECTION("negative") {
+        SECTION("overflow") {
+            double val = -std::numeric_limits<double>::max();
+            TestType converted = static_cast<TestType>(val);
+            REQUIRE(converted == UB_handle::to_integer_conversion::NEG_OVERFLOW<TestType>);
+        }
+        SECTION("infinity") {
+            double val = -std::numeric_limits<double>::infinity();
+            TestType converted = static_cast<TestType>(val);
+            REQUIRE(converted == UB_handle::to_integer_conversion::NEG_INF<TestType>);
+        }
+        SECTION("NaN") {
+            SECTION("quiet") {
+                double val = -std::numeric_limits<double>::quiet_NaN();
+                TestType converted = static_cast<TestType>(val);
+                REQUIRE(converted == UB_handle::to_integer_conversion::NaN<TestType>);
+            }
+            SECTION("signaling") {
+                double val = -std::numeric_limits<double>::signaling_NaN();
+                TestType converted = static_cast<TestType>(val);
+                REQUIRE(converted == UB_handle::to_integer_conversion::NaN<TestType>);
+            }
+        }
+    }
+}
 
 TEMPLATE_TEST_CASE("test utils", "[utils]", float, double, quadruple) {
-
     SECTION("test repeatability") {
         SECTION("normal") {
             auto first_gen = generate_normal_numbers<TestType>(test_size);
